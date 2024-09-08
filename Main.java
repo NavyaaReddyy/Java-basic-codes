38eef8524d5528297bc614c3a3bad6772cefc0a8
NavyaaReddyy <125690352+NavyaaReddyy@users.noreply.github.com>
2024-09-08 00:15:15 -0400

    Add files via upload
    Odd-Even

--- Main.java
+++ Main.java
@@ -1,23 +1,16 @@
-// conditions if-else
 import java.util.*;
 public class Main{
-    public static void main(String args[]) {
-        Scanner sc = new Scanner (System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
 
-        if (a==b){
-            System.out.println("Equal");
-        }    else {
-            if (a>b){
-                System.out.println("a is greater");
+        if(x%2 == 0){
+            System.out.println("Even");
 
-            }
-            else {
-                System.out.println("a is lesser");
-            }
-
+        }else {
+            System.out.println("odd");
         }
 
     }
+
 }
